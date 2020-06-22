d4a8770f07359c6580479deb5e0bc0023c2d7491
Etsh <H79Pain@gmail.com>
2020-06-22 15:02:41 +0200

    trying out layout widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MiCard());
+}
+
+class MiCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            color: Colors.teal,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  color: Colors.red,
+                  width: 100.0,
+                  height: double.infinity,
+                ),
+                Container(
+                  color: Colors.yellow,
+                  height: 100.0,
+                  width: 100.0,
+                ),
+                Container(
+                  color: Colors.blue,
+                  height: double.infinity,
+                  width: 100.0,
+                ),
+              ],
+              // mainAxisSize: ,
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
